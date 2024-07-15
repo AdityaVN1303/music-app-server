2e4c0f2d68b5e56e44670f3db83d2243eaa0481e
Aditya <adityanagare09@gmail.com>
2024-07-15 21:30:07 +0530

    minor change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import cookieParser from "cookie-parser";
 
 // app config
 const app = express();
-app.use(cors({credentials : true , origin : 'https://music-app-client-five.vercel.app'}));
+app.use(cors({credentials : true , origin : 'https://music-app-client-five.vercel.app/'}));
 app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 connectCloudinary();
